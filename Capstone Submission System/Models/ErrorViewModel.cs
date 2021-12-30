09d6282a6d6fa9833b813622ce8799444620fe7b
Skerdilajd Skenderi <skerdiskenderi98@gmail.com>
2021-12-30 14:42:08 -0500

    Add project files.

---
+++ Capstone Submission System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Capstone_Submission_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
